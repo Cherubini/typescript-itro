549e572db57a1aa9e0a3c2b9aac7c8a52804e0d7
Cherubini <127298157+Cherubini@users.noreply.github.com>
2023-05-09 12:07:12 +0200

    aggiunta di interfacce

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { UtilityService } from 'src/app/services/utility/utility.service';
 import { Todo } from 'src/app/model/todo';
+import { DataService } from 'src/app/services/data/data.service';
+import { ITodo } from 'src/app/model/i-todo';
 
 @Component({
   selector: 'app-todo-list',
@@ -8,13 +10,19 @@ import { Todo } from 'src/app/model/todo';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent {
- todoArray: Todo[] = []
+ todoArray: ITodo[] = []
 
-  constructor(private utilityService: UtilityService){
-  const todo1 = new Todo('comprare il pane', false);
-  const todo2 = new Todo('buttare la spazzatura', false);
-  const todo3 = new Todo('pagare le bollette', false);
-  this.todoArray =[todo1, todo2, todo3]
+  constructor(private utilityService: UtilityService, private dataS: DataService){
+  // const todo1 = new Todo('comprare il pane', false);
+  // const todo2 = new Todo('buttare la spazzatura', false);
+  // const todo3 = new Todo('pagare le bollette', false);
+  //this.todoArray =[todo1, todo2, todo3]
+  this.dataS.getData().then(data => {
+    console.log(data);
+    this.todoArray = data
+    console.log(this.todoArray);
+
+  })
   }
 
   sortTodo():void{
